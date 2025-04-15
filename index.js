f5d2072a91539d2dc66fe373c17f8d1ac01687ef
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-04-15 12:21:11 +0200

    commited new

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+const port = 3000;
+
+const app = express();
+app.use(cors);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
