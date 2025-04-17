aaeb9f95418e9622d5ae895cde14e79bcf64c0a1
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-04-17 09:12:46 +0200

    borrar id

--- index.js
+++ index.js
@@ -3,7 +3,33 @@ import cors from "cors";
 const port = 3000;
 
 const app = express();
-app.use(cors);
+app.use(cors());
+app.use(express.json());
+
+const animals = [
+  { id: 1, name: "gatp", strength: 3 },
+  { id: 2, name: "Elefante", strength: 8 },
+  { id: 3, name: "Murciel", strength: 5 },
+];
+app.get("/animals", (req, res) => {
+  res.send(animals);
+});
+
+app.post("/animals", (req, res) => {
+  animals.push({ name: req.body.name, strength: req.body.strength });
+  res.sendStatus(201);
+});
+
+app.delete("/animals/:id", (req, res) => {
+  const idToDelete = Number(req.params.id);
+  const filteredAnimals = animals.filter((animal) => animal.id !== idToDelete);
+  if (filteredAnimals.length == animals.length) {
+    res.status(404).send("Ningun elemento borrado");
+  } else {
+    animals = filteredAnimals;
+    res.sendStatus(200);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
